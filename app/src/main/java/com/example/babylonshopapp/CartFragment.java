a176587c2a1e869a152277b04c4b29d33a98ca00
majd <ashour.majd@gmail.com>
2020-12-05 22:48:26 +0300

    checkout updated..

--- app/src/main/java/com/example/babylonshopapp/CartFragment.java
+++ app/src/main/java/com/example/babylonshopapp/CartFragment.java
@@ -6,27 +6,39 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
 
 public class CartFragment extends Fragment {
 
-
+    TextView itemprice1 , itemprice2, totalprice;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_cart, container, false);
+
+        itemprice1 = v.findViewById(R.id.itemprice1);
+        itemprice2 = v.findViewById(R.id.itemprice2);
+        totalprice = v.findViewById(R.id.totalPrice);
+
+        Double TotalPrice = Double.parseDouble(itemprice1.getText().toString()) + Double.parseDouble(itemprice2.getText().toString()) ;
+
+        totalprice.setText(""+ TotalPrice);
+
         Button checkoutbtn = v.findViewById(R.id.checkoutbtn);
         checkoutbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getActivity().getApplicationContext(),CheckoutActivity.class);
-                i.putExtra("price", "price3142");
+                i.putExtra("Total_Price", "" + TotalPrice);
                 startActivity(i);
             }
         });
+
         return v;
     }
 
